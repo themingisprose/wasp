de28ed66924266f8469829c888dda927e4d4584e
rogertm <roger213tm@gmail.com>
2021-09-02 15:45:08 -0400

    Class WASP_Custom_Post_Type

--- classes/index.php
+++ classes/index.php
@@ -7,3 +7,6 @@
 require plugin_dir_path( __FILE__ ) .'/admin/class.wasp-admin-page.php';
 require plugin_dir_path( __FILE__ ) .'/admin/class.wasp-admin-sub-page.php';
 require plugin_dir_path( __FILE__ ) .'/admin/class.wasp-admin-setting-fields.php';
+
+// Custom Post Type classes
+require plugin_dir_path( __FILE__ ) .'/post-type/class.wasp-post-type.php';
