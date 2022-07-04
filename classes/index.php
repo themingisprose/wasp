2feb8ec86c76325f709e54e490018c701166c8e1
rogertm <roger213tm@gmail.com>
2022-07-04 17:02:10 -0400

    Organizado los includes en el directorio classes/

--- classes/index.php
+++ classes/index.php
@@ -4,18 +4,16 @@
  */
 
 // Admin classes
-require plugin_dir_path( __FILE__ ) .'/admin/class.wasp-admin-setting-fields.php';
-require plugin_dir_path( __FILE__ ) .'/admin/class.wasp-admin-page.php';
-require plugin_dir_path( __FILE__ ) .'/admin/class.wasp-admin-sub-page.php';
+require plugin_dir_path( __FILE__ ) .'/admin/index.php';
 
 // Custom Post Type classes
-require plugin_dir_path( __FILE__ ) .'/post-type/class.wasp-post-type.php';
+require plugin_dir_path( __FILE__ ) .'/post-type/index.php';
 
 // Custom Taxonomies
-require plugin_dir_path( __FILE__ ) .'/taxonomy/class.wasp-taxonomy.php';
+require plugin_dir_path( __FILE__ ) .'/taxonomy/index.php';
 
 // Meta Boxes
-require plugin_dir_path( __FILE__ ) .'/meta-box/class.wasp-meta-box.php';
+require plugin_dir_path( __FILE__ ) .'/meta-box/index.php';
 
 // General Plugin classes
-require plugin_dir_path( __FILE__ ) .'/general/class.wasp-enqueue.php';
+require plugin_dir_path( __FILE__ ) .'/general/index.php';
--- classes/meta-box/index.php
+++ classes/meta-box/index.php
@@ -2,3 +2,4 @@
 /**
  * Silent is gold
  */
+require plugin_dir_path( __FILE__ ) .'class.wasp-meta-box.php';
