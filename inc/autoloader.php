3e48620aa623389f4840f2f2503258418cf50dfe
rogertm <roger213tm@gmail.com>
2023-10-10 16:18:19 -0400

    Renombrados todos los archivos de clases de class.* a class-* Cumpliendo con los estándares de código de WordPress

--- inc/autoloader.php
+++ inc/autoloader.php
@@ -8,7 +8,7 @@ spl_autoload_register( function( $class ){
 	$directories = glob( plugin_dir_path( __DIR__ ) .'classes/*', GLOB_ONLYDIR );
 
 	foreach ( $directories as $dir ) :
-		$file = $dir .'/class.'. str_replace( '_', '-', strtolower( $class ) ) .'.php';
+		$file = $dir .'/class-'. str_replace( '_', '-', strtolower( $class ) ) .'.php';
 
 		if ( file_exists( $file ) ) :
 			require_once $file;
